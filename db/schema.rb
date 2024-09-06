a9cb7c743dd48848d2bfbb0a1cb866b3e709deb2
Robin BORG <robinborg.pro@gmail.com>
2024-09-06 23:19:34 +0200

    ajout des cars pour les team, modification du schema et de la seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_27_155053) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_06_192245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_27_155053) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "captain_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,4 +61,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_27_155053) do
 
   add_foreign_key "team_markers", "markers"
   add_foreign_key "team_markers", "teams"
+  add_foreign_key "teams", "users", column: "captain_id"
 end
