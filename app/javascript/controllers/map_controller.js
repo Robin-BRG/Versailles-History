ff66e6b5b3485524390d97ad8d67ffd65268050b
bastien penard <bastien.penard@gmail.com>
2024-09-18 21:56:53 +0200

    La map s'affiche, mais le cercle toujours pas...

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,8 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
+// import L from "leaflet"; // L est l'alias utilisé par Leaflet
 
-// Connects to data-controller="map"
 export default class extends Controller {
+  static targets = [ "map" ]
   connect() {
-    console.log("Map controller connected");
+    console.log("Map connected")
+
+    const L = window.L;
+    this.map = L.map(this.mapTarget).setView([48.8049, 2.1204], 14);
+
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    L.marker([48.8049, 2.1204]).addTo(this.map)
+    .bindPopup('Un point d\'exemple.')
+    .openPopup();
+    L.circle([48.8049, 2.1204], { radius: 2000 }).addTo(this.map);
   }
 }
