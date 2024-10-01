5e4403979039b18331d6d5cb782396b598b03986
Robin BORG <robinborg.pro@gmail.com>
2024-10-01 16:35:53 +0200

    sgajout de la logique pour rejoindre / quitter /créer /supprimer une équipe

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,19 +1,46 @@
 class TeamsController < ApplicationController
   before_action :check_existing_team, only: [:create]
+  before_action :set_team, only: [:destroy, :join, :leave]
+
+  def index
+    @teams = Team.all
+  end
 
   def create
     @team = Team.new(team_params)
     @team.captain = current_user
     if @team.save
-      respond_to do |format|
-        format.html { redirect_to team_path, notice: 'Équipe créée avec succès.' }
-        format.json { render json: { message: 'Équipe créée avec succès.' }, status: :created }
-      end
+      redirect_to teams_path, notice: 'Équipe créée avec succès.'
     else
-      respond_to do |format|
-        format.html { render :new }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
+      render :new, alert: 'Erreur lors de la création de l\'équipe.'
+    end
+  end
+
+  def destroy
+    if @team.captain == current_user
+      @team.team_markers.destroy_all
+      @team.destroy
+      redirect_to teams_path, notice: "Équipe supprimée avec succès."
+    else
+      redirect_to teams_path, alert: "Vous n'êtes pas autorisé à supprimer cette équipe."
+    end
+  end
+
+  def join
+    if current_user.team.nil? && @team.users.count < 8
+      current_user.update(team: @team)
+      redirect_to teams_path, notice: "Vous avez rejoint l'équipe avec succès."
+    else
+      redirect_to teams_path, alert: 'Impossible de rejoindre cette équipe.'
+    end
+  end
+
+  def leave
+    if current_user.team == @team
+      current_user.update(team: nil)
+      redirect_to teams_path, notice: "Vous avez quitté l'équipe avec succès."
+    else
+      redirect_to teams_path, alert: "Impossible de quitter cette équipe."
     end
   end
 
@@ -25,10 +52,11 @@ class TeamsController < ApplicationController
 
   def check_existing_team
     if current_user.team.present?
-      respond_to do |format|
-        format.html { redirect_to team_path, alert: 'Vous avez déjà une équipe.' }
-        format.json { render json: { message: 'Vous avez déjà une équipe.' }, status: :forbidden }
-      end
+      redirect_to teams_path, alert: 'Vous avez déjà une équipe.'
     end
+  end
+
+  def set_team
+    @team = Team.find(params[:id])
   end
 end
