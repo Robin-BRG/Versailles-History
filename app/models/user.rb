5ea29675043e59a5a9bcaf10a682544af9fca0bf
Robin BORG <robinborg.pro@gmail.com>
2024-10-04 14:11:17 +0200

    ajout de la generation aleatoire des ponts / Design de l'index team fini / Ajout de la modal gallery

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,33 @@ class User < ApplicationRecord
 
 
   # Permet de créer une équipe pour l'utilisateur au moment de la création du profil. L'equipe est crée sans nom car la view team n'affiche que les équipes avec un nom.
-  after_create :create_team
+  after_create :create_team_and_markers
 
   private
 
-  def create_team
+  def create_team_and_markers
     team = Team.create(captain: self)
-    self.update(team: team) # Assurez-vous que l'utilisateur est mis à jour avec l'équipe
+    self.update(team: team)
+
+    markers = Marker.order(:id).to_a
+    hotel_le_louis = markers.find { |marker| marker.name == "Hôtel Le Louis" }
+    markers.delete(hotel_le_louis)
+    markers.shuffle!
+    markers << hotel_le_louis
+
+    markers.each_with_index do |marker, index|
+      TeamMarker.create!(
+        team_id: team.id,
+        marker_id: marker.id,
+        order: index,
+        visited: false,
+        circle_center_latitude: random_in_range(marker.latitude - 0.001, marker.latitude + 0.001).clamp(-90, 90),
+        circle_center_longitude: random_in_range(marker.longitude - 0.001, marker.longitude + 0.001).clamp(-180, 180),
+      )
+    end
+  end
+
+  def random_in_range(min, max)
+    (rand * (max - min)) + min
   end
 end
